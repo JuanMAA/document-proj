9c5f8ac05db01ee66efa7ea2dac6aa24cd1cec1c
jaime <james18bt@DESKTOP-CNDNDFB.localdomain>
2021-03-30 21:57:56 -0300

    tabla incorporada lol

--- src/index.js
+++ src/index.js
@@ -3,16 +3,16 @@ import ReactDOM from "react-dom";
 import { createBrowserHistory } from "history";
 import { Router, Route, Switch } from "react-router-dom";
 
-import LandingPage from './views/landing-page/landing-page';
-import Menu from './components/menu/menu';
-import MenuDashboard from './components/menu/menu-dashboard';
+import LandingPage from "./views/landing-page/landing-page";
+import Menu from "./components/menu/menu";
+import MenuDashboard from "./components/menu/menu-dashboard";
 
 var hist = createBrowserHistory();
 
 ReactDOM.render(
   <Router history={hist}>
     <Switch>
-      <Menu/>
+      <Menu />
       <Route path="/" component={LandingPage} />
     </Switch>
     <Switch>
@@ -21,4 +21,3 @@ ReactDOM.render(
   </Router>,
   document.getElementById("root")
 );
-
